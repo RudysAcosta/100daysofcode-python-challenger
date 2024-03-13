21ee147d3eb6cc342c4aa2b25d9cb001cc6d2baa
Rudys Natanael Acosta Crousset <ncrousset926@gmail.com>
2024-03-12 21:44:34 -0400

    refactory main method

--- blind-auction/main.py
+++ blind-auction/main.py
@@ -1,18 +1,25 @@
+import os
 from art import logo
 
 bidders = []
 
 def main():
+    print(logo)
     print("Welcome to the secret auction program. ")
-    bidder = 'yes'
-    while bidder == 'yes':
+    while True:
         get_bidder_details()
-        bidder = input("Are there any other bidders? Type 'yes' or 'no'. \n")
-
-    winner = get_winner()
-    print(f"The winner is {winner['name'].capitalize()} with a bid of ${winner['bid']}")
-    print()
-
+        while True:
+            bidder = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+            if bidder not in ['no', 'yes']:
+                print("Invalid input. Please try again.")
+                continue
+            if bidder == 'yes':
+                break
+            if bidder == 'no':
+                winner = get_winner()
+                print(f"The winner is {winner['name'].capitalize()} with a bid of ${winner['bid']}")
+                exit()
+            
 def get_winner():
     return max(bidders, key=lambda bidder: bidder['bid'])
 
@@ -21,6 +28,7 @@ def get_bidder_details():
     bid = float(input("What's your bid?:  "))
     bidder = { 'name': name, 'bid': bid }
     bidders.append(bidder)
+    os.system("clear")
 
 if __name__ == "__main__":
     main()
